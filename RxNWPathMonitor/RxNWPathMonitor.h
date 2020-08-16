4c98d4999071a6df37463dd2b32550528ac7b811
Fumito Ito <fumito.ito@nex.nikkei.com>
2020-08-16 23:18:37 +0900

    initial commit

---
+++ RxNWPathMonitor/RxNWPathMonitor.h
@@ -0,0 +1,19 @@
+//
+//  RxNWPathMonitor.h
+//  RxNWPathMonitor
+//
+//  Created by 伊藤史 on 2020/08/16.
+//  Copyright © 2020 Fumito Ito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxNWPathMonitor.
+FOUNDATION_EXPORT double RxNWPathMonitorVersionNumber;
+
+//! Project version string for RxNWPathMonitor.
+FOUNDATION_EXPORT const unsigned char RxNWPathMonitorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxNWPathMonitor/PublicHeader.h>
+
+
